6ead650d3e38d626e8fea4bf7b752f8db1e98526
Teng Li <otec.teng@gmail.com>
2015-11-12 18:02:28 +0800

    add a fault

--- spec/models/bird_spec.rb
+++ spec/models/bird_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Bird, :type => :model do
   it "should pass" do
-    expect(Bird.new().can_fly?).to eql(true)
+    expect(Bird.new().can_fly?).to eql(false)
   end
 end
